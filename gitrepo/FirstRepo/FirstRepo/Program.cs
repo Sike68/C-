a3fb5cccc6fe78a264b195965a9e7ebe464dedb0
Sike68 <160533197+Sike68@users.noreply.github.com>
2025-10-19 00:48:38 +0600

    Add files via upload

---
+++ gitrepo/FirstRepo/FirstRepo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Fahim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 2, c, d;
+            c = add(a, b);
+            Console.WriteLine($"{c}");
+            d = add(a, b, c);
+            Console.WriteLine($"{d}");
+            Console.WriteLine($"{add(10.5f, 25.6f)}");
+            Console.ReadKey();
+        }
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+        static int add(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static float add(float x, float y)
+        {
+            return x + y;
+        }
+    }
+}
