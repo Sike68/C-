237a8dc72def2bb82299ae7ff3a49321b63ef187
Sike68 <160533197+Sike68@users.noreply.github.com>
2025-10-21 01:26:14 +0600

    Add files via upload

---
+++ gitrepo/FirstRepo/FirstRepo/FirstRepo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Fahim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            Emp1.EmployeeID = 1001;
+            Emp1.Name = "James";
+
+            UpdateName(ref Emp1);
+            Console.WriteLine($"Emp1 Name = {Emp1.Name}");
+            Console.ReadKey();
+        }
+
+        public static void UpdateName(ref Employee Emp2)
+        {
+            Emp2 = null;
+        }
+    }
+    public class Employee
+    {
+        public int EmployeeID;
+        public string Name;
+    }
+}
